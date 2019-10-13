b3147006000a2c43b28cd2149a07528c4350e613
peternu2020 <39267532+peternu2020@users.noreply.github.com>
2019-10-13 16:46:31 -0500

    Components Cards and Cart DropDown Option Change

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -9,17 +9,8 @@ const ShoppingCart = ({ cartInfoContent, removeFromCart }) => {
     for (i = 0; i < cartInfoContent.length; i += 1) {
         const content = cartInfoContent[i];
         const product = content.product;
-        if (content['S'] !== 0) {
-            contents.push([product, 'S', content['S']]);
-        }
-        if (content['M'] !== 0) {
-            contents.push([product, 'M', content['M']]);
-        }
-        if (content['L'] !== 0) {
-            contents.push([product, 'L', content['L']]);
-        }
-        if (content['XL'] !== 0) {
-            contents.push([product, 'XL', content['XL']]);
+        if (content['Going'] !== 0) {
+            contents.push([product, 'Going', content['Going']]);
         }
     }
 
@@ -31,7 +22,7 @@ const ShoppingCart = ({ cartInfoContent, removeFromCart }) => {
                 </Column>
                 {contents.map(content =>
                     <Column size='full' key={content[0].sku}>
-                        <CartItem product={content[0]} size={content[1]} amount={content[2]} removeFromCart={removeFromCart} />
+                        <CartItem product={content[0]} going={content[1]} amount={content[2]} removeFromCart={removeFromCart} />
                     </Column>
                 )}
             </Column.Group>
