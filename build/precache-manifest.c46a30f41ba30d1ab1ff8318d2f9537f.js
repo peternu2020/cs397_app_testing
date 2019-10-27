fde82db83d5e342fb5a2865981cb96958db167d8
Zachary McKee <zmckee@u.northwestern.edu>
2019-10-26 20:59:46 -0500

    removed node_modules from git because i'm tired of removing it and reinstalling every time

---
+++ build/precache-manifest.c46a30f41ba30d1ab1ff8318d2f9537f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2746499ae560f0ae7b7ffc997a3020c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d855dd7a3ea5de3795cd",
+    "url": "/static/css/2.9cbe54d5.chunk.css"
+  },
+  {
+    "revision": "a6bdeae86358114d48d9",
+    "url": "/static/css/main.a38eb3d3.chunk.css"
+  },
+  {
+    "revision": "d855dd7a3ea5de3795cd",
+    "url": "/static/js/2.d3c060f0.chunk.js"
+  },
+  {
+    "revision": "a6bdeae86358114d48d9",
+    "url": "/static/js/main.ad3e0e57.chunk.js"
+  },
+  {
+    "revision": "cfd85b4678887566b37e",
+    "url": "/static/js/runtime-main.a58aba83.js"
+  }
+]);
