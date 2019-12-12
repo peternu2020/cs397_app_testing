751c78bd2447c861601770303cfaf4dda4ce9d25
peternu2020 <39267532+peternu2020@users.noreply.github.com>
2019-12-11 22:52:59 -0600

    Add files via upload

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -5,4 +5,9 @@ describe ('Test App', () => {
   it ('launches', () => {
     cy.visit ('/');
   });
+
+  it ('opens with All Days', () => {
+    cy.visit ('/');
+    cy.get('[data-cy=day_filter]').should('contain', 'All Days');
+  });
 });
